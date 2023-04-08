c97000f5aabde01c74a4c89b20c78d5adb11ff9a
Collins Rotich <14885955+collinskrotich@users.noreply.github.com>
2023-04-08 08:29:14 +0300

    2nd api

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,56 @@
-import React from 'react'
+import React, {Component} from 'react'
 
-export const index = () => {
-  return (
-    <div>index</div>
-  )
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [],
+      isLoaded: false
+    }
+  }
+
+
+componentDidMount() {
+
+  fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(json => {
+      this.setState({
+        isLoaded: true,
+        items: json,
+      })
+    });
 }
+
+render() {
+  var { isLoaded, items } = this.state;
+
+  if (!isLoaded) {
+    return <div>Loading...</div>
+  }
+  
+  else {
+    return (
+      <div className='Appp'>
+
+        <ul>
+          {items.map(item => (
+            <li key={item.id}>
+              Name: {item.name} |||
+              Email: {item.email}
+
+            </li>
+          ))}
+        </ul>
+       
+
+
+       </div>
+    )
+  }
+
+  }
+}
+
+export default App;
